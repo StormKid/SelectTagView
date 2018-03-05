d050a21af300464a0d33df8ea3f3fa608fd7f9c2
ke_li <stokid@126.com>
2018-03-05 18:13:47 +0800

    首次提交

---
+++ selecttagview/src/main/java/com/stormkid/selecttagview/SelectTagView.kt
@@ -0,0 +1,216 @@
+package com.stormkid.selecttagview
+
+import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.stormkid.selecttagview.util.DimenUtil
+
+/**
+ * Created by ke_li on 2018/2/28.
+ */
+class SelectTagView : ViewGroup {
+
+    private var textSize = 0 // tag字体大小
+
+    private var textColor = 0 // tag字体颜色
+
+    private var backGroundColor = 0 // tag背景色
+
+    private var backGroundResource = 0 // tag背景布局
+
+    private var type = NORMAL_CLOUD_TYPE
+
+    companion object {
+        const val NORMAL_CLOUD_TYPE = "NORMAL_CLOUD_TYPE" // 普通的操作方式
+        const val SINGLE_SELECT_TYPE = "SINGLE_SELECT_TYPE" // 单选
+        const val MORE_SELECT_TYPE = "MORE_SELECT_TYPE" // 多选
+    }
+
+    constructor(context: Context) : this(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    /**
+     * init对象
+     */
+    constructor(context: Context, attrs: AttributeSet?, def: Int) : super(context, attrs, def) {
+        val attr = context.obtainStyledAttributes(attrs!!, R.styleable.SelectTagView, def, 0)
+        textSize = attr.getInt(R.styleable.SelectTagView_tagTextSize, 8)
+        textColor = attr.getResourceId(R.styleable.SelectTagView_tagTextColor, R.color.text_666)
+        backGroundColor = attr.getResourceId(R.styleable.SelectTagView_tagBgColor, android.R.color.white)
+        backGroundResource = attr.getResourceId(R.styleable.SelectTagView_tagBgColor, 0)
+        val xmlType= attr.getInt(R.styleable.SelectTagView_tagSelectType, 0)
+        when (xmlType){
+            0-> type= NORMAL_CLOUD_TYPE
+            1-> type = SINGLE_SELECT_TYPE
+            2-> type = MORE_SELECT_TYPE
+        }
+    }
+
+    /**
+     * 使用系统自带margin params
+     */
+    override fun generateLayoutParams(attrs: AttributeSet?): LayoutParams {
+        return MarginLayoutParams(context, attrs)
+    }
+
+
+    /**
+     * 更新type是否为多选或单选或者只点击
+     */
+    fun setType(type:String){
+        this.type = type
+    }
+
+    /**
+     * 控制子view所在的位置
+     */
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        // 初始化子控件位置
+        var pointWidth = 0
+        var pointHeight = 0
+
+        (0 until childCount)
+                .asSequence() //序列化
+                .map { getChildAt(it) }//开始遍历子控件
+                .filter { it.visibility != View.GONE } // 过滤view为gone的控件
+                .forEach {
+                    // 获取子控件的测量宽高
+                    val childHeight = it.measuredHeight
+                    val childWidth = it.measuredWidth
+                    // 使用margin的params作为定位器
+                    val layoutParams = it.layoutParams as MarginLayoutParams
+                    // 判断是否换行。
+                    if (pointWidth + childWidth + layoutParams.leftMargin + layoutParams.rightMargin > width) {
+                        pointHeight += childHeight + layoutParams.topMargin + layoutParams.bottomMargin
+                        pointWidth = 0
+                    }
+                    // 计算控件绘图定位
+                    val top = layoutParams.topMargin + pointHeight
+                    val bottom = layoutParams.bottomMargin + pointHeight + childHeight
+                    val left = layoutParams.leftMargin + pointWidth
+                    val right = layoutParams.rightMargin + pointWidth + childWidth
+                    it.layout(left, top, right, bottom)
+                    // 通过view的tag来显示view的实际变化
+                    val tag = it.tag as CateGroyBean
+                    if (tag.isChoose) it.setBackgroundResource(R.drawable.shape_selected)
+                    else it.setBackgroundResource(R.drawable.shape_no_select)
+                    //记录最终view的位置
+                    pointWidth += layoutParams.leftMargin + childWidth + layoutParams.rightMargin
+                }
+
+    }
+
+
+    /**
+     * 计算子控件大小进行自动换行处理
+     */
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val sizeWidth = MeasureSpec.getSize(widthMeasureSpec)
+        val sizeHeight = MeasureSpec.getSize(heightMeasureSpec)
+        val modeWidth = MeasureSpec.getMode(widthMeasureSpec)
+        val modeHeight = MeasureSpec.getMode(heightMeasureSpec)
+
+        //初始化父控件大小
+        var resultWidth = 0
+        var resultHeight = 0
+
+
+        // 初始化行控件大小
+        var itemWidth = 0
+        var itemHeight = 0
+
+        for (i in 0 until childCount) { // 遍历 所有的子元素
+            val child = getChildAt(i)
+            val layoutParams = child.layoutParams as MarginLayoutParams
+            measureChild(child, widthMeasureSpec, heightMeasureSpec) // 先测量
+
+            //计算所有的子控件宽高
+            val childWidth = child.measuredWidth
+            val childHeight = child.measuredHeight
+            // 通过margin计算所有子控件实际加上margin值的大小
+            val realWidth = childWidth + layoutParams.leftMargin + layoutParams.rightMargin
+            val realHeight = childHeight + layoutParams.topMargin + layoutParams.bottomMargin
+
+            if (sizeWidth < (itemWidth + realWidth)) {//换行
+                resultWidth = Math.max(realWidth, itemWidth)
+                resultHeight += realHeight
+                itemHeight = realHeight
+                itemWidth = realWidth
+            } else { // 添加
+                itemWidth += realWidth
+                itemHeight = Math.max(realHeight, itemHeight)
+            }
+
+            // 最后一行不换行
+            if (i == childCount - 1) {
+                resultWidth = Math.max(realWidth, itemWidth)
+                resultHeight += itemHeight
+            }
+            // 通过判断本自定义控件width||height 的属性是否为warp_content来进行给此view赋值，如果为march_parent或者其他属性，则使用其他属性定义的宽高值
+            // 如果仅为wrap_content则使用计算后的宽高给父控件赋值
+            setMeasuredDimension(if (modeWidth == View.MeasureSpec.EXACTLY) sizeWidth else resultWidth,
+                    if (modeHeight == View.MeasureSpec.EXACTLY) sizeHeight else resultHeight)
+        }
+    }
+
+    fun initChild(list: MutableList<CateGroyBean>,callbcak:(MutableList<CateGroyBean>)->Unit) = list.apply {
+        removeAllViews()
+    }
+            .forEachIndexed { position, categoryBean ->
+                val textView = TextView(context)
+                textView.setTextColor(ContextCompat.getColor(context, textColor))
+                DimenUtil.getInstance(context).getSmallSize(textView)
+                textView.setBackgroundColor(ContextCompat.getColor(context, backGroundColor))
+                textView.tag = categoryBean
+                val layoutParams = MarginLayoutParams(ViewGroup.MarginLayoutParams.WRAP_CONTENT, ViewGroup.MarginLayoutParams.WRAP_CONTENT)
+                val margin = DimenUtil.getInstance(context).getWindowWidth() / 45
+                layoutParams.rightMargin = margin
+                layoutParams.leftMargin = margin
+                layoutParams.bottomMargin = margin
+                layoutParams.topMargin = margin
+                textView.layoutParams = layoutParams
+                textView.text = categoryBean.name
+                if (backGroundResource != 0) textView.setBackgroundResource(backGroundResource)
+                textView.setPadding(margin * 2, margin, margin * 2, margin)
+                textView.setOnClickListener {
+                    //通过数据绑定来控制显示
+                    when(type){
+                        SINGLE_SELECT_TYPE ->{ //单选
+                            categoryBean.isChoose = !categoryBean.isChoose
+                            it.tag = categoryBean
+                            list.forEachIndexed { index, categaryBean ->
+                                if (position!=index){
+                                    categaryBean.isChoose = false
+                                    getChildAt(index).tag = categaryBean
+                                }
+                            }
+                        }
+                        MORE_SELECT_TYPE ->{ // 多选
+                            categoryBean.isChoose = !categoryBean.isChoose
+                            it.tag = categoryBean
+                        }
+                        NORMAL_CLOUD_TYPE ->{ //普通选定
+                            categoryBean.isChoose = true
+                            it.tag = categoryBean
+                            list.forEachIndexed { index, categaryBean ->
+                                if (position!=index){
+                                    categaryBean.isChoose = false
+                                    getChildAt(index).tag = categaryBean
+                                }
+                            }
+                        }
+                    }
+                    requestLayout()
+                    callbcak.invoke(list)
+                }
+                addView(textView)
+            }
+
+    data class CateGroyBean(val id:String, val name:String, var isChoose:Boolean)
+}
---
+++ selecttagview/src/main/java/com/stormkid/selecttagview/util/DimenUtil.kt
@@ -0,0 +1,82 @@
+package com.stormkid.selecttagview.util
+
+import android.content.Context
+import android.util.TypedValue
+import android.widget.TextView
+
+/**
+ * Created by ke_li on 2017/12/4.
+ */
+class DimenUtil private constructor(context: Context) {
+
+    private val myContext = context
+
+    private val DEFAULT_SMALL_SIZE = 8
+
+    private val DEFAULT_SIZE = 10
+
+    private val DEFAULT_BIG_SIZE = 12
+
+    private val DEFAULT_PADDING = 12
+
+    private val DEFAULT_MARGIN = 12
+
+    companion object {
+        @JvmStatic
+        fun getInstance(context: Context): DimenUtil {
+            return DimenUtil.inner.util(context)
+        }
+    }
+
+    private object inner {
+        val util = fun(context: Context): DimenUtil = DimenUtil(context)
+        val width = fun(context: Context): Int { return context.resources.displayMetrics.widthPixels }
+    }
+
+
+    fun getWindowWidth(): Int {
+        return inner.width(myContext)
+    }
+
+
+    fun getResultSize(size: Int): Float {
+        val window = getWindowWidth()
+        val caculated = window * size / 250f
+        return caculated
+    }
+
+    fun getDimen(size: Int):Int{
+        return Math.round(getResultSize(size))
+    }
+
+    fun getDimen(size: Int,textView: TextView) =  Math.round(getResultSize(size)).apply {
+        setTextSize(this,textView)
+    }
+
+
+    fun setTextSize(size: Int, textView: TextView) {
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResultSize(size))
+    }
+
+
+
+    fun getDefaltSize(textView: TextView) {
+        setTextSize(DEFAULT_SIZE, textView)
+    }
+
+    fun getBigSize(textView: TextView) {
+        setTextSize(DEFAULT_BIG_SIZE, textView)
+    }
+
+    fun getSmallSize(textView: TextView) {
+        setTextSize(DEFAULT_SMALL_SIZE, textView)
+    }
+
+    fun getDefaltPadding(): Int {
+        return Math.round(getResultSize(DEFAULT_PADDING))
+    }
+
+    fun getDefaltMarging(): Int {
+        return Math.round(getResultSize(DEFAULT_MARGIN))
+    }
+}
